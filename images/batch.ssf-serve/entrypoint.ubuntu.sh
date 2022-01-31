e2058dfc4677a3fe8622a26594a017d424d898e0
SuzukiGithub <>
2022-02-01 02:08:21 +0900

    [stones-55/stones-survey-form#187] image : batch - serve

---
+++ images/batch.ssf-serve/entrypoint.ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+echo "whoami            : `whoami`"
+echo "date              : `date`"
+echo "CRONTAB_FILE_PATH : ${CRONTAB_FILE_PATH}"
+
+crontab ${CRONTAB_FILE_PATH}
+echo "crontab -l"
+crontab -l
+
+cron -f -l 2
